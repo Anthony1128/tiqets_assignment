4dae7fccaf32ccdb29154a09d86f693756116531
anthony <antonwowis@yandex.ru>
2023-04-19 00:57:31 +0300

    fix todo and requirements

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from etl import extract_job, load_job, transform_job
 
-# TODO make config
-BARCODES_PATH = "barcodes.csv"
-ORDERS_PATH = "orders.csv"
-
 
 class DataProcessor:
     """Main class to process the data."""
 
     def __init__(self):
         self.destination = "result.csv"
+        self.barcodes_path = "barcodes.csv"
+        self.orders_path = "orders.csv"
 
     def run(self):
         """Entrypoint of data processing."""
-        df_barcodes = extract_job.read_csv_file(BARCODES_PATH)
-        df_orders = extract_job.read_csv_file(ORDERS_PATH)
+        df_barcodes = extract_job.read_csv_file(self.barcodes_path)
+        df_orders = extract_job.read_csv_file(self.orders_path)
         df_transformed = transform_job.transform_data(df_barcodes, df_orders)
         load_job.save_locally(df_transformed, self.destination)
 
